add4bfcbc449cdbadd4f2880e39fa62718e22017
Ebrahim <akbrly42@gmail.com>
2021-09-18 17:23:10 +0430

    update product list

--- src/Project/techno-market/components/items.js
+++ src/Project/techno-market/components/items.js
@@ -7,7 +7,8 @@ class Items extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://run.mocky.io/v3/a8d03157-a077-44db-9746-695e18a7549e')
+        // axios.get('https://run.mocky.io/v3/a8d03157-a077-44db-9746-695e18a7549e')
+        axios.get('data.json')
             .then(res => {
                 this.setState({ products: res.data.products })
             })
@@ -22,6 +23,7 @@ class Items extends Component {
                             <div className="item bg-white shadow d-flex flex-column justify-content-between align-items-center p-2">
                                 <img src={product.image} alt="" className="mb-2" />
                                 <span className="text-center">{product.name}</span>
+                                <span>{product.amount}</span>
                                 <span>
                                     <span className="text-danger">{product.price}</span>
                                     <span> تومان</span>
