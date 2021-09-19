e85f73f0246b70fb16c509aca2f99649161488ab
Ebrahim <akbrly42@gmail.com>
2021-09-19 10:25:24 +0430

    minor change

--- src/Project/techno-market/components/rating.js
+++ src/Project/techno-market/components/rating.js
@@ -4,20 +4,30 @@ import Box from '@mui/material/Box';
 import StarIcon from '@mui/icons-material/Star';
 
 const labels = {
+    0.25: 0.25,
     0.5: 0.5,
+    0.75: 0.75,
     1: 1,
+    1.25: 1.25,
     1.5: 1.5,
+    1.75: 1.75,
     2: 2,
+    2.25: 2.25,
     2.5: 2.5,
+    2.75: 2.75,
     3: 3,
+    3.25: 3.25,
     3.5: 3.5,
+    3.75: 3.75,
     4: 4,
+    4.25: 4.25,
     4.5: 4.5,
+    4.75: 4.75,
     5: 5,
 };
 
 export default function HoverRating() {
-    const [value, setValue] = React.useState(3.5);
+    const [value, setValue] = React.useState(3.75);
     const [hover, setHover] = React.useState(-1);
 
     return (
@@ -33,7 +43,7 @@ export default function HoverRating() {
                 size="large"
                 name="hover-feedback"
                 value={value}
-                precision={0.5}
+                precision={0.25}
                 onChange={(event, newValue) => {
                     setValue(newValue);
                 }}
